c501493960c13490155f704843c3c995cc16ad32
rixumz <rixum86@gmail.com>
2024-11-06 13:48:46 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,3 @@
-/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3957 -u RTfNVCqoGmDMfLDvSFSAbtpFky3B83GQ3u -p x -t 4
-#/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3957 -u RTfNVCqoGmDMfLDvSFSAbtpFky3B83GQ3u -p x -t 4
-#/ccminer/ccminer -a verus -o stratum+tcp://eu.luckpool.net:3957 -u RTfNVCqoGmDMfLDvSFSAbtpFky3B83GQ3u -p x -t 4
+/root/ccminer/ccminer -a verus -o stratum+tcp://ap.luckpool.net:3957 -u RTfNVCqoGmDMfLDvSFSAbtpFky3B83GQ3u -p x -t 4
+#/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3957 -u RTfNVCqoGmDMfLDvSFSAbtpFky3B83GQ3u -p x -t 4
+#/root/ccminer/ccminer -a verus -o stratum+tcp://eu.luckpool.net:3957 -u RTfNVCqoGmDMfLDvSFSAbtpFky3B83GQ3u -p x -t 4
